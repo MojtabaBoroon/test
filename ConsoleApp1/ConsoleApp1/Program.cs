d6c7dfda54051c0267bb48dbf61649741cb4f009
Mojtaba Boroon <mb1298@gmail.com>
2022-05-31 20:52:06 +0200

    add lines

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,12 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello !");
+            Console.WriteLine("Hello !");
+            Console.WriteLine("Hello !");
+            Console.WriteLine("Hello !");
+            Console.WriteLine("Hello !");
+            Console.WriteLine("Hello !");
+            Console.WriteLine("Hello !");
             Console.ReadLine();
         }
     }
